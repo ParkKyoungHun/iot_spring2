716312d4e1504fe3b37d4a8ba4b2fd992200f1ce
DJA <DJA@DJA-PC>
2017-08-24 17:11:43 +0900

    t

--- iot_spring2/src/main/java/com/iot/sp2/user/service/UserService.java
+++ iot_spring2/src/main/java/com/iot/sp2/user/service/UserService.java
@@ -6,18 +6,5 @@ import java.util.Map;
 import com.iot.sp2.user.dto.UserInfo;
 
 public interface UserService {
-
-	public UserInfo getUserPwd(Map paramMap);
-
-	public int insertUser(Map paramMap);
-	
-	public List memlist();
-	
-	public List memrole();	
-	
-	public int memdel(Map paramMap);
-	
-	public int saveDeleteUser(Map paramMap);
-	
 	public UserInfo getUser(UserInfo pUser);
 }
--- iot_spring2/src/main/java/com/iot/sp2/user/service/UserServiceImpl.java
+++ iot_spring2/src/main/java/com/iot/sp2/user/service/UserServiceImpl.java
@@ -1,88 +1,20 @@
 package com.iot.sp2.user.service;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.iot.sp2.common.dao.MainDao;
 import com.iot.sp2.user.dao.UserDao;
 import com.iot.sp2.user.dto.UserInfo;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
-	@Autowired
-	private MainDao mainDao;
 	@Autowired
 	private UserDao userDao;
-	
-	public UserInfo getUserPwd(Map pm) {
-		String sqlId = "userinfo.SELECT_USER";
-		UserInfo user = (UserInfo) mainDao.getObject(sqlId, pm);
-		boolean checkPwd = checkPwd(pm, user);
-		if(!checkPwd){
-			return null;
-		}
-		return user;
-	}
-
-	
 	public UserInfo getUser(UserInfo pUser) {
 		UserInfo user = (UserInfo) userDao.selectUser(pUser);
 		if(user!=null && user.getUserPwd().equals(pUser.getUserPwd())){
 			return user;
 		}
 		return null;
-	}
-	public boolean checkPwd(Map pm, UserInfo ud){
-		if(ud==null){
-			return false;
-		}
-		String userPwd = (String)pm.get("userPwd");
-		String checkPwd = ud.getUserPwd();
-		
-		if(userPwd.equals(checkPwd)){
-			return true;
-		}
-		return false;
-	}
-
-	public List memlist(){
-		String sqlId="user.USER_LIST";
-		List memlist= mainDao.getList(sqlId);
-		return memlist;
-	}
-	
-
-	public List memrole(){
-		String sqlId="user.USER_ROLE";
-		List memrole= mainDao.getList(sqlId);
-		return memrole;
-	}
-	
-	
-	public int memdel(Map pm){
-		String sqlId = "user.USER_DEL";
-		int user = mainDao.memdel(sqlId, pm);
-		return user;
-	}
-
-	@Override
-	public int insertUser(Map pm) {
-		String sqlId = "user.USER_INSERT";
-		int result = mainDao.memdel(sqlId, pm);
-		return result;
-	}
-
-	@Override
-	public int saveDeleteUser(Map pm) {
-		String sqlId = "user.USER_DEL";
-		int result = mainDao.memdel(sqlId, pm);
-		if(result==1){
-			throw new  RuntimeException("걍 에러!");
-		}
-		return result;
 	}
 }
